a12a8ffc0d537fb74d31fc5dc1e763a5deda9fc8
Bryan Cantrill <bryan@oxide.computer>
2020-07-20 01:13:15 -0700

    introduce operation to flush any batched writes
    With probe's batching writes, there is a need for higher-level
    software to flush any buffered writes.  This assures that (say)
    operations like run() do not run the risk of returning to the caller
    with their operations not sent to the MCU.  (This issue was the
    reason for failures in SWO processing on the LPC55, as higher level
    software's resume of the halted part was not, in fact, resuming
    the part.)

--- probe-rs/src/architecture/arm/memory/adi_v5_memory_interface.rs
+++ probe-rs/src/architecture/arm/memory/adi_v5_memory_interface.rs
@@ -511,6 +511,13 @@ where
 
         Ok(())
     }
+
+    pub fn flush(&mut self) -> Result<(), AccessPortError> {
+        match self.interface.flush() {
+            Ok(_) => Ok(()),
+            Err(e) => Err(AccessPortError::FlushError(e)),
+        }
+    }
 }
 
 /// Calculates a 32-bit word aligned range from an address/length pair.
@@ -568,6 +575,10 @@ where
 
     fn write_8(&mut self, address: u32, data: &[u8]) -> Result<(), Error> {
         ADIMemoryInterface::write_8(self, address, data).map_err(Error::architecture_specific)
+    }
+
+    fn flush(&mut self) -> Result<(), Error> {
+        ADIMemoryInterface::flush(self).map_err(Error::architecture_specific)
     }
 }
 
